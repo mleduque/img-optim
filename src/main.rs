ad4a9918d0a253c19f5d16d5008b319f1dffa8b7
Mickaël <mleduque@gmail.com>
2021-04-27 15:20:46 +0200

    unpack/repack cbz

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use anyhow::anyhow;
 use anyhow::{Result};
 use clap::Clap;
 use glob::glob;
+use path_absolutize::*;
 use std::path::Path;
 use std::process::Command;
 
@@ -15,8 +16,63 @@ struct Opts {
 
 fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
+    let dir = tempfile::tempdir()?;
+    let out_dir = tempfile::tempdir()?;
+    println!("temp dirs ok");
+    let target_zip = Path::new(&opts.target).absolutize()?;
+    println!("absolutize ok");
+
+    unpack_archive(&Path::new(&opts.source).absolutize()?.to_owned(), &dir)?;
+    let pattern = match dir.path().to_str() {
+        Some(path) => path.to_owned() + "/*",
+        None => {
+            println!("invalid temp file path {:?}", dir.path());
+            return Err(anyhow!("invalid temp file path"));
+        }
+    };
+    println!("unpack ok");
+    process_files(&pattern, &out_dir.path())?;
+    println!("process ok");
+    repack_output(&out_dir, &target_zip)
+}
+
+fn unpack_archive(zip_path: &Path, tmp_dir: &tempfile::TempDir) -> Result<()> {
+    let zip_file = std::fs::File::open(&zip_path)?;
+    let mut archive = zip::ZipArchive::new(zip_file)?;
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        let out_path = match file.enclosed_name() {
+            Some(path) => path.to_owned(),
+            None => return Err(anyhow!("invalid name for file in archive: {:?}", file.mangled_name())),
+        };
+
+        if (&*file.name()).ends_with('/') {
+            std::fs::create_dir_all(tmp_dir.path().join(&out_path))?;
+        } else {
+            if let Some(p) = out_path.parent() {
+                if !p.exists() {
+                    std::fs::create_dir_all(tmp_dir.path().join(&p))?;
+                }
+            }
+            let mut out_file = std::fs::File::create(tmp_dir.path().join(&out_path))?;
+            std::io::copy(&mut file, &mut out_file)?;
+        }
+                // Get and Set permissions
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+
+            if let Some(mode) = file.unix_mode() {
+                std::fs::set_permissions(tmp_dir.path().join(&out_path), std::fs::Permissions::from_mode(mode))?;
+            }
+        }
+    }
+    Ok(())
+}
+
+fn optimize_images(source_glob: &str, target_dir: &str) -> Result<()> {
     
-    let target = Path::new(&opts.target);
+    let target = Path::new(target_dir);
     if target.exists() {
         if target.is_dir() {
             // continue only if it's empty
@@ -40,12 +96,16 @@ fn main() -> Result<()> {
         std::fs::create_dir(target)?;
     }
 
-    for entry in glob(&opts.source)? {
+    process_files(source_glob, target)
+}
+
+fn process_files(source_glob: &str, target: &Path) -> Result<()> {
+    for entry in glob(&source_glob)? {
         println!("{:?}", entry);
         match entry {
             Ok(ref path) => {
                 if path.is_file() {
-                    match path.canonicalize() {
+                    match path.absolutize() {
                         Ok(canon) => match process_one_file(&canon, &target) {
                                         Ok(_) => {}
                                         Err(error) => {
@@ -54,7 +114,7 @@ fn main() -> Result<()> {
                                         }
                                     }
                         Err(error) => {
-                            println!("couldn't canonicalize path {:?} - {:?}", entry, error);
+                            println!("couldn't absolutize path {:?} - {:?}", entry, error);
                         }
                     }
                 } else {
@@ -89,4 +149,10 @@ fn process_one_file(item: &Path, target: &Path) -> Result<()> {
     } else {
         Err(anyhow!("path{} has no filename, skipping", item.to_string_lossy()))
     }
+}
+fn repack_output(dir: &tempfile::TempDir, zip: &Path) -> Result<()> {
+    use zip::CompressionMethod;
+    let options = zip::write::FileOptions::default().compression_method(CompressionMethod::Stored);
+    zip_extensions::write::zip_create_from_directory_with_options(&zip.to_owned(), &dir.path().to_owned(), options)?;
+    Ok(())
 }
